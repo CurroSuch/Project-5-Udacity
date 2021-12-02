da8273022573c4652b86d5bba6a1cc09cd09b7be
Francisco Miguel Such Llinás <68127766+CurroSuch@users.noreply.github.com>
2021-12-02 22:22:21 +0100

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -4,6 +4,7 @@ import { handleSubmit } from './js/formHandler'
 
 
 const button = document.getElementById('submit-button');
+button.addEventListener('click', checkForName)
 
 async function postData(){
     let response = await fetch("http://localhost:9000/sentiment-analysis", {
@@ -21,6 +22,8 @@ setData(result)
 return result;
 }
 
+
+
 function setData(result) {
     let dataresult = document.getElementById('juanito');
     console.log(dataresult)
@@ -28,4 +31,12 @@ function setData(result) {
 };
 
 const button2 = document.getElementById('submit-button-2')
-button2.addEventListener('click', postData);
+button2.addEventListener('click', function getSentenceValue() {
+    const sentence = document.getElementById('sentence').value
+    if(sentence !== "" && isNaN(sentence) !== false){
+        postData();;
+    } else{
+        const error = document.getElementById('juanito')
+        error.innerText = "Your value is not valid!"
+    }
+})
